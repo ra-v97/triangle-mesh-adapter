faad1b79dd3022db72257f1ce7a762ce36868f1a
greg9702 <grzegorzb9702@gmail.com>
2020-12-28 20:18:14 +0100

    Removed BeforeEach decorated method

--- src/test/java/transformations/TransformationP3Test.java
+++ src/test/java/transformations/TransformationP3Test.java
@@ -11,31 +11,13 @@ import pl.edu.agh.gg.model.Vertex;
 import pl.edu.agh.gg.transformations.TransformationP3;
 import utils.GraphTestUtils;
 
+import java.util.Collection;
+
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TransformationP3Test {
-
-    private TransformationP3 transformation;
-
-    private GraphModel graphModel;
-
-    private InteriorNode initialInteriorNode;
-
-    @BeforeEach
-    private void initializeTestCase() {
-        transformation = new TransformationP3();
-        graphModel = createValidLeftSideGraph();
-        initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
-                .orElseThrow(IllegalStateException::new);
-    }
-
-    private void reloadGraphModel(GraphModel model) {
-        graphModel = model;
-        initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
-                .orElseThrow(IllegalStateException::new);
-    }
 
     private static GraphModel createLeftSideGraph(Coordinates c1, Coordinates c2, Coordinates c3, Coordinates c4) {
         final GraphModel graphModel = new GraphModel();
@@ -128,32 +110,55 @@ public class TransformationP3Test {
 
     @Test
     void transformationShouldExecuteForCorrectLeftSide() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertTrue(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldExecuteWhenDifferentVerticesOrder() {
-        // When
-        reloadGraphModel(createLeftSideGraph(
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraph(
                 new Coordinates(1, -1, 0),
                 new Coordinates(0, 0, 0),
                 new Coordinates(1, 1, 0),
-                new Coordinates(1, 0, 0)));
+                new Coordinates(1, 0, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertTrue(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteWhenInteriorLabelIsLower() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // When
         initialInteriorNode.setLabel("i");
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteAfterRemovingAdjacentVertexFromLeftSide() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // When
         graphModel.removeVertex(graphModel.getVertices()
                 .stream()
@@ -164,6 +169,12 @@ public class TransformationP3Test {
 
     @Test
     void transformationShouldNotExecuteAfterRemovingFourthVertexFromLeftSide() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // When
         graphModel.removeVertex(graphModel.getVertices()
                 .stream()
@@ -174,6 +185,12 @@ public class TransformationP3Test {
 
     @Test
     void transformationShouldNotExecuteAfterRemovingEdgeFromLeftSide() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // When
         graphModel.removeEdge(graphModel.getEdges().stream()
                 .filter(edge -> edge.getType() == GraphEdge.GraphEdgeType.VERTEX_VERTEX).findFirst().get().getUUID());
@@ -183,66 +200,247 @@ public class TransformationP3Test {
 
     @Test
     void transformationShouldNotExecuteWhenAdditionalVertexIsNotOnTheLongestEdge() {
-        // When
-        reloadGraphModel(createLeftSideGraphWithAdditionalVertexOnWrongEdge());
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraphWithAdditionalVertexOnWrongEdge();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteWhenAdditionalVertexIsNotInTheMiddle() {
-        // When
-        reloadGraphModel(createLeftSideGraph(new Coordinates(1, 1, 0),
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraph(new Coordinates(1, 1, 0),
                 new Coordinates(-2, 0, 0),
                 new Coordinates(1, -1, 0),
-                new Coordinates(0.5, 0, 0)));
+                new Coordinates(0.5, 0, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteWhenAdditionalVertexOverlapsWithAdjVertex() {
-        // When
-        reloadGraphModel(createLeftSideGraphWithThreeVertices(new Coordinates(1, 1, 0),
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraphWithThreeVertices(new Coordinates(1, 1, 0),
                 new Coordinates(1, 0, 0),
-                new Coordinates(1, -1, 0)));
+                new Coordinates(1, -1, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteWhenTwoAdditionalVerticesArePresent() {
-        // When
-        reloadGraphModel(createLeftSideGraphWithTwoAdditionalVertices(new Coordinates(1, 1, 0),
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraphWithTwoAdditionalVertices(new Coordinates(1, 1, 0),
                 new Coordinates(0, 0, 0),
                 new Coordinates(1, -1, 0),
                 new Coordinates(1, 0, 0),
-                new Coordinates(0.5, 0.5, 0)));
+                new Coordinates(0.5, 0.5, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldNotExecuteWhenWrongZCord() {
-        // When
-        reloadGraphModel(createLeftSideGraph(
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraph(
                 new Coordinates(1, -1, 1),
                 new Coordinates(0, 0, 0),
                 new Coordinates(1, 1, 0),
-                new Coordinates(1, 0, 0)));
+                new Coordinates(1, 0, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertFalse(transformation.isApplicable(graphModel, initialInteriorNode));
     }
 
     @Test
     void transformationShouldExecuteWhenGraphIsBiggerButValid() {
-        // When
-        reloadGraphModel(createLeftSideGraphWithFivelVertices(new Coordinates(1, 1, 0),
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createLeftSideGraphWithFivelVertices(new Coordinates(1, 1, 0),
                 new Coordinates(0, 0, 0),
                 new Coordinates(1, -1, 0),
                 new Coordinates(1, 0, 0),
-                new Coordinates(2, 0, 0)));
+                new Coordinates(2, 0, 0));
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
         // Then
         assertTrue(transformation.isApplicable(graphModel, initialInteriorNode));
+    }
+
+    @Test
+    void baseTransformationNumberOfInteriors() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // When
+        transformation.transform(graphModel, initialInteriorNode);
+
+        // Then
+        assertEquals(3, graphModel.getInteriors().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfEdgesBeforeTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // Then
+        assertEquals(7, graphModel.getEdges().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfEdgesAfterTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // When
+        transformation.transform(graphModel, initialInteriorNode);
+
+        // Then
+        assertEquals(20, graphModel.getEdges().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfInteriorsBeforeTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // Then
+        assertEquals(1, graphModel.getInteriors().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfInteriorsAfterTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // When
+        transformation.transform(graphModel, initialInteriorNode);
+
+        // Then
+        assertEquals(3, graphModel.getInteriors().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfNodesBeforeTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // Then
+        assertEquals(4, graphModel.getVertices().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfNodesAfterTransformation() {
+        // Given
+        var transformation = new TransformationP3();
+        var graphModel = createValidLeftSideGraph();
+        var initialInteriorNode = GraphTestUtils.resolveGraphInterior(graphModel)
+                .orElseThrow(IllegalStateException::new);
+
+        // When
+        transformation.transform(graphModel, initialInteriorNode);
+
+        // Then
+        assertEquals(8, graphModel.getVertices().toArray().length);
+    }
+
+    @Test
+    void baseTransformationNumberOfNodesAfterTransformationXD() {
+        var graphModel = createValidLeftSideGraph();
+        assertTrue(isEqual(graphModel, graphModel));
+    }
+
+    static boolean isEqual(GraphModel g1, GraphModel g2) {
+        if (g2 == null) {
+            return false;
+        }
+        Collection<Vertex> vl1 = g1.getVertices();
+        Collection<Vertex> vl2 = g2.getVertices();
+        Collection<InteriorNode> il1 = g1.getInteriors();
+        Collection<InteriorNode> il2 = g2.getInteriors();
+        Collection<GraphEdge> el1 = g1.getEdges();
+        Collection<GraphEdge> el2 = g2.getEdges();
+
+        for (Vertex v1 : vl1) {
+            boolean found = false;
+            for (Vertex v2 : vl2) {
+                if (v1.getCoordinates().equals(v2.getCoordinates())) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+
+        for (InteriorNode i1 : il1) {
+            boolean found = false;
+            for (InteriorNode i2 : il2) {
+                if (i1.getCoordinates().equals(i2.getCoordinates())) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                return false;
+        }
+
+        for (GraphEdge e1 : el1) {
+            boolean found = false;
+            for (GraphEdge e2 : el2) {
+                var e1e = e1.getEdgeNodes();
+                var e2e = e2.getEdgeNodes();
+                if (e1e.getValue0().getCoordinates().equals(e2e.getValue1().getCoordinates()) || (e1e.getValue0().getCoordinates().equals(e2e.getValue0().getCoordinates()))) {
+                    if (e1e.getValue1().getCoordinates().equals(e2e.getValue1().getCoordinates()) || (e1e.getValue1().getCoordinates().equals(e2e.getValue0().getCoordinates()))) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            if (!found)
+                return false;
+        }
+
+        return true;
     }
 }
