bb2f34559833450ab936d541a97c7920e9b38138
Rafal Stachura <rafstach.97@gmail.com>
2021-01-12 18:50:29 +0100

    Christmas cleaning

--- src/main/java/pl/edu/agh/gg/app/ExerciseRegistry.java
+++ src/main/java/pl/edu/agh/gg/app/ExerciseRegistry.java
@@ -2,40 +2,59 @@ package pl.edu.agh.gg.app;
 
 import pl.edu.agh.gg.common.Coordinates;
 import pl.edu.agh.gg.common.LayerDescriptor;
-import pl.edu.agh.gg.model.GraphEdge;
 import pl.edu.agh.gg.model.GraphModel;
 import pl.edu.agh.gg.model.InteriorNode;
-import pl.edu.agh.gg.model.Vertex;
 import pl.edu.agh.gg.transformations.*;
-import pl.edu.agh.gg.transformations.TransformationP5;
 import pl.edu.agh.gg.visualization.Visualizer;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
+public final class ExerciseRegistry {
 
-public class Main {
-    public static void main(String[] args) {
+    public static void executeTaskB() {
         final GraphModel graph = createStartingGraph();
-        List<Transformation> transformations = Arrays.asList(new TransformationP1(), new TransformationP2());
+        Visualizer visualizer = new Visualizer(graph);
+        visualizer.visualize();
+        System.out.println("Starting graph" + " Press any key to continue");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        for (int i = 0; i < 1; i++) { // this will be replaced with a do-while loop when we have the logic for refining the triangles
+        ArrayList<Transformation> transformations = new ArrayList<>();
+        transformations.add(new TransformationP1());
+        transformations.add(new TransformationP2());
+        transformations.add(new TransformationP9());
+
+        for (int i = 0; i < 2; i++) { // this will be replaced with a do-while loop when we have the logic for refining the triangles
             InteriorNode[] interiors = graph.getInteriors().toArray(new InteriorNode[0]);
             for (InteriorNode interior : interiors) {
                 for (Transformation t : transformations) {
                     if (t.isApplicable(graph, interior)) {
                         System.out.println("Executing transformation: " + t.getClass().getSimpleName() + " on interior" + interior.getLabel());
                         t.transform(graph, interior);
+                        transformations.remove(t);
+                        visualizer = new Visualizer(graph);
+                        visualizer.visualize(new LayerDescriptor(0));
+                        visualizer.visualize(new LayerDescriptor(1));
+                        visualizer.visualize(new LayerDescriptor(2));
+                        System.out.println("Graph after " + t.getClass().getSimpleName() + " Press any key to continue");
+                        try {
+                            System.in.read();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
                     }
                 }
             }
         }
 
-        List<DoubleInteriorTransformation> doubleInteriorTransformations =
-                Arrays.asList(new TransformationP6(), new TransformationP7());
+        ArrayList<DoubleInteriorTransformation> doubleInteriorTransformations = new ArrayList<>();
+        doubleInteriorTransformations.add(new TransformationP10());
+
         InteriorNode[] interiors = graph.getInteriors().toArray(new InteriorNode[0]);
         for (DoubleInteriorTransformation t : doubleInteriorTransformations) {
             for (InteriorNode interior1 : interiors) {
@@ -48,16 +67,19 @@ public class Main {
             }
         }
 
-        final Visualizer visualizer = new Visualizer(graph);
-//        visualizer.visualize();
+        visualizer = new Visualizer(graph);
         visualizer.visualize(new LayerDescriptor(0));
         visualizer.visualize(new LayerDescriptor(1));
-//        visualizer.visualize(new LayerDescriptor(2));
+        visualizer.visualize(new LayerDescriptor(2));
+        System.out.println("Final graph (after TransformationP10)");
     }
 
     private static GraphModel createStartingGraph() {
         final GraphModel graphModel = new GraphModel();
         graphModel.insertStartingInterior("S", new LayerDescriptor(0), new Coordinates(0, 0, 0));
         return graphModel;
+    }
+
+    private ExerciseRegistry() {
     }
 }
